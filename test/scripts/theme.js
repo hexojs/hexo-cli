14e51440858ca56e1e0e69add2ac4c61f4db3077
Stanley Sathler <stanleysathlerpinto@gmail.com>
2018-10-10 00:20:47 -0300

    Added test to test theme folder structure creation

--- test/scripts/theme.js
+++ test/scripts/theme.js
@@ -1,28 +1,63 @@
 'use strict';
 
 var should = require('chai').should(); // eslint-disable-line
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var rewire = require('rewire');
+var sinon = require('sinon');
 var Context = require('../../lib/context');
 
 describe('theme', function() {
-  let context;
-  let themeModule;
+  var context;
+  var themeModule;
 
   beforeEach(function() {
     context = new Context();
-    themeModule = (require('../../lib/console/theme'));
+    themeModule = require('../../lib/console/theme');
   });
 
   it('throw error when folder already exists', function() {
     var params = { _: [] };
 
-    themeModule
+    return themeModule
       .call(context, params)
       .catch(function(error) {
         error.message.should.contain('A theme name must be provided');
       });
   });
 
-  it('create folder structure', function() {
-    //
+  it('create theme initial structure', function() {
+    var params = { _: ['my-hexo-theme'] };
+    var themeModule = rewire('../../lib/console/theme');
+    var mkdirSpy = sinon.spy();
+    var writeFileSpy = sinon.spy();
+    var fsMock = {
+      mkdirSync: mkdirSpy,
+      writeFileSync: writeFileSpy,
+    };
+
+    return themeModule
+      .__with__({ fs: Object.assign(fs, fsMock) })
+      (function() {
+        return themeModule.call(context, params);
+      })
+      .then(function() {
+        mkdirSpy.args.should.eql([
+          [path.join(context.base_dir, 'my-hexo-theme')],
+          [path.join(context.base_dir, 'my-hexo-theme/languages')],
+          [path.join(context.base_dir, 'my-hexo-theme/layout')],
+          [path.join(context.base_dir, 'my-hexo-theme/scripts')],
+          [path.join(context.base_dir, 'my-hexo-theme/source')],
+        ]);
+
+        writeFileSpy.args.should.eql([
+          [path.join(context.base_dir, 'my-hexo-theme/config.yml'), ''],
+          [path.join(context.base_dir, 'my-hexo-theme/languages/.gitkeep'), ''],
+          [path.join(context.base_dir, 'my-hexo-theme/layout/.gitkeep'), ''],
+          [path.join(context.base_dir, 'my-hexo-theme/scripts/.gitkeep'), ''],
+          [path.join(context.base_dir, 'my-hexo-theme/source/.gitkeep'), ''],
+        ]);
+      });
   });
 });
